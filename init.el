d955d056345f42899cfa273f699a825df6e27c4d
flowmis <hedwig.lanter@gmx.de>
2022-01-28 14:55:36 +0100

    new repo

---
+++ init.el
@@ -0,0 +1,54 @@
+;;init.el ist das was beim Start von Emacs geladen wird und das hässliche Emacs in ein schönes mit Themes ändert und Funktionalität hinzufügt
+
+;;Aussehen
+(setq inhibit-startup-message t) ;M-x eval-buffer (um den aktuellen buffer zu checken auf errors
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+(menu-bar-mode -1)          ; Disable menu bar
+(setq visible-bell t)       ;emacs leuchtet visuell (Rahmen) auf
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 280)  ;andere Schrift wählen da diese nicht funzt
+(load-theme 'wombat)
+
+;; ESC quit all prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+(use-package command-log-mode)
+
+;;Autovervollständigung (ivy oder helmet)
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
