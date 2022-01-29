962c43fa7f7ffd6210a91130451c8bdffd34d3fc
flowmis <hedwig.lanter@gmx.de>
2022-01-29 13:50:31 +0100

    new

--- nonrootinstall.sh
+++ nonrootinstall.sh
@@ -6,6 +6,8 @@
  cp ~/FlowmisOS/packages.el ~/.config/doom/packages.el
  cp ~/FlowmisOS/config.el ~/.config/doom/config.el
  cp ~/FlowmisOS/.xprofile ~/.xprofile
+ cp ~/FlowmisOS/config.py ~/.config/qtile/config.py
+ cp ~/FlowmisOS/config.rasi ~/.config/rofi/config.rasi
  ~/.emacs.d/bin/doom sync
  cd ~
  git clone https://aur.archlinux.org/brave-bin.git
--- rootinstall.sh
+++ rootinstall.sh
@@ -5,6 +5,7 @@
  echo LANG=de_DE.UTF-8 | cat > /etc/locale.conf
  sed -i 's/#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/g' /etc/locale.gen
  locale-gen
+ touch /etc/hostname
  echo FlowmisPC | cat /etc/hostname
  echo 127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\tFlowmisPC.localdomain\tFlowmisPC | cat > /etc/hosts
  pacman -S grub pcmanfm efibootmgr dosfstools os-prober mtools networkmanager xf86-video-fbdev xorg alacritty base-devel lightdm lightdm-gtk-greeter picom nitrogen qtile rofi emacs
@@ -12,4 +13,3 @@
  mount /dev/sda1 boot/EFI
  grub-install /dev/sda
  grub-mkconfig -o /boot/grub/grub.cfg
- exit
