3634ad409ed0b0b44c7761619ad74eb299b8b638
flowmis <hedwig.lanter@gmx.de>
2022-01-30 14:40:10 +0100

    check

--- tangle/rootinstall.sh
+++ tangle/rootinstall.sh
@@ -1,14 +1,16 @@
    usermod -aG wheel,audio,video,optical,storage flowmis                      # Erteilen der Rechte bzw. in welcher Gruppe der User ist
-   sed -i 's/ #%wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers      # Ermöglicht sudo usern der Gruppe wheel alle sudo Befehle auszuführen wenn sie das sudo passwort haben
    ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime                    # Link zur Zeitzone um richtige Uhrzeit etc. zu hinterlegen. Manche Programme funktionieren ohne richtige locales nicht
    hwclock --systohc                                                          # setzt Zeit
-   mv /home/flowmis/FlowmisOS/locale.conf /etc/locale.conf                    # verschiebt die hier im Dokument erstellte Datei locale.conf mit richtigem Inhalt an den richtigen Ort
+   mv /home/flowmis/FlowmisOS/tangle/locale.conf /etc/locale.conf                    # verschiebt die hier im Dokument erstellte Datei locale.conf mit richtigem Inhalt an den richtigen Ort
    sed -i 's/#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/g' /etc/locale.gen          # sucht einen String und ersetzt ihn
    locale-gen                                                                 # generiert die locales
-   echo FlowmisPC | cat > /etc/hostname                                       # schreibt neue Datei an gewünschten Ort mit gewünschtem Inhalt (touch nötig wenn Datei noch nicht vorhanden ist?)
-   mv /home/flowmis/FlowmisOS/hosts /etc/hosts
+   echo FlowmisPC | cat > /etc/hostname                                       # schreibt neue Datei an gewünschten Ort mit gewünschtem Inhalt
+   mv /home/flowmis/FlowmisOS/tangle/hosts /etc/hosts
    pacman -S grub efibootmgr dosfstools os-prober mtools networkmanager xf86-video-fbdev
    mkdir /boot/EFI
    mount /dev/sda1 boot/EFI
    grub-install /dev/sda
    grub-mkconfig -o /boot/grub/grub.cfg
+   cd /home/flowmis
+   sudo chown flowmis FlowmisOS                                                  # owner root -> owner flowmis
+   sudo chgrp flowmis FlowmisOS                                                  # Gruppe root -> Gruppe flowmis
--- tangle/verschieben.sh
+++ tangle/verschieben.sh
@@ -1,7 +1,13 @@
-mkdir -p ~/.config/{rofi,alacritty,picom,qtile,doom}
-cp -r /home/flowmis/FlowmisOS/init.el /home/flowmis/.config/doom/init.el
-cp -r /home/flowmis/FlowmisOS/packages.el /home/flowmis/.config/doom/packages.el
-cp -r /home/flowmis/FlowmisOS/config.el /home/flowmis/.config/doom/config.el
-cp -r /home/flowmis/FlowmisOS/.xprofile /home/flowmis/.xprofile
-cp -r /home/flowmis/FlowmisOS/config.py /home/flowmis/.config/qtile/config.py
-cp -r /home/flowmis/FlowmisOS/config.rasi /home/flowmis/.config/rofi/config.rasi
+mkdir -p ~/.config/{rofi,alacritty,picom,qtile}
+# doom conky dmlight pcmanfm ,...???
+#cp -r ~/FlowmisOS/tangle/init.el ~/.config/doom/init.el
+#cp -r ~/FlowmisOS/tangle/packages.el ~/.config/doom/packages.el
+#cp -r ~/FlowmisOS/tangle/config.el ~/.config/doom/config.el
+cp -r ~/FlowmisOS/tangle/.xprofile ~/.xprofile
+cp -r ~/FlowmisOS/tangle/.bashrc ~/.bashrc
+#cp -r ~/FlowmisOS/tangle/.gitconfig ~/.gitconfig
+#cp -r ~/FlowmisOS/tangle/.git-credentials ~/.git-credentials
+cp -r ~/FlowmisOS/tangle/picom.conf ~/.config/picom/picom.conf
+cp -r ~/FlowmisOS/tangle/config.rasi ~/.config/rofi/config.rasi
+cp -r ~/FlowmisOS/tangle/config.py ~/.config/qtile/config.py
+cp -r ~/FlowmisOS/tangle/alacritty.yml ~/.config/alacritty/alacritty.yml
