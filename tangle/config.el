79d1b53e0cb83989fa049a52a5c44fbbe6dc4f82
flowmis <hedwig.lanter@gmx.de>
2022-05-21 23:27:35 +0200

    banner angepasst

--- tangle/config.el
+++ tangle/config.el
@@ -134,15 +134,13 @@
   :init      ;; tweak dashboard config before loading it
   (setq dashboard-set-heading-icons t)
   (setq dashboard-set-file-icons t)
-  (setq dashboard-banner-logo-title "\nKEYBINDINGS:\nOpen dired file manager  (SPC .)\nOpen buffer list         (SPC b i)\nFind recent files        (SPC f r)\nOpen the eshell          (SPC e s)\nToggle big font mode     (SPC t b)")
   ;;(setq dashboard-startup-banner 'logo) ;; use standard emacs logo as banner
-  (setq dashboard-startup-banner "~/.config/doom/banner.png")  ;; use custom image as banner
+  (setq dashboard-startup-banner "~/.config/doom/banner/banner.png")  ;; use custom image as banner
   (setq dashboard-center-content nil) ;; set to 't' for centered content
   (setq dashboard-items '((recents . 5)
-                          (agenda . 5 )
-                          (bookmarks . 5)
-                          (projects . 5)
-                          (registers . 5)))
+                          (agenda . 5)
+                          (bookmarks . 3)
+                          (projects . 3)))
   :config
   (dashboard-setup-startup-hook)
   (dashboard-modify-heading-icons '((recents . "file-text")
