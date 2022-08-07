ab52850226f377258de478e70a3c0f92fd15db81
flowmis <hedwig.lanter@gmx.de>
2022-08-07 18:08:40 +0200

    ditaa zu install

--- tangle/install.sh
+++ tangle/install.sh
@@ -25,5 +25,5 @@ cp -rf ~/.config/rofi/rofi/fonts/* ~/.local/share/fonts/
 cp -rf ~/.config/rofi/rofi/1080p/* ~/.config/rofi/
 cd ~/.config/rofi/
 rm -rf rofi/
-sudo pacman -S python-utils brightnessctl xorg-xbacklight acpi xfce4-power-manager systemd i3lock scrot viewnior dunst bind bmon nm-connection-editor network-manager-applet aspell aspell-de aspell-en
-yay -S date termite the-silver_searcher
+sudo pacman -S python-utils brightnessctl xorg-xbacklight acpi xfce4-power-manager systemd i3lock scrot viewnior dunst bind bmon nm-connection-editor network-manager-applet aspell aspell-de aspell-en the-silver_searcher ditaa
+yay -S date termite
