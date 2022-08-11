37b41e9e451ac9a0839340d3193188520cc67e15
flowmis <hedwig.lanter@gmx.de>
2022-08-11 22:29:26 +0200

    meiste Dinge funktionieren -> org roam hinbekommen

--- tangle/config.el
+++ tangle/config.el
@@ -1,70 +1,68 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-(setq user-full-name "Markus Hoffmann"
-      user-mail-address "manemarkushoffmann@gmail.com" ;GPG configuration, email clients, file templates and snippets,... can use this information - ist optional
-      eshell-rc-script "~/.config/doom/eshell/profile"
-      eshell-aliases-file "~/.config/doom/eshell/aliases"
-      eshell-buffer-maximum-lines 5000
-      eshell-scroll-to-bottom-on-input t)
 (after! org
-  (setq
-      org-directory "~/Dropbox/emacs/org-roam/Notizen/"
-      org-agenda-files '("~/Dropbox/emacs/org-roam/Notizen/orga/agenda.org")
-      org-default-notes-file (expand-file-name "notes.org" org-directory)
-      org-ellipsis " ▼ "
-      org-log-done 'time
-      org-journal-dir "~/Dropbox/emacs/org-roam/Notizen/orga/journal/"
-      org-journal-date-format "%B %d, %Y (%A) "
-      org-journal-file-format "%Y-%m-%d.org"
-      org-hide-emphasis-markers t ;;macht dass Markierungssymbole um kursiv, dick, unterstrichen,... zu schreiben (~-_/*=) ausgeblendet werden
-      org-link-abbrev-alist    ; This overwrites the default Doom org-link-abbrev-list welche man mit <K> aufrufen kann
-        '(("google" . "http://www.google.com/search?q=")                ;default
-          ("doom-repo" . "https://github.com/doomemacs/doomemacs/%s")   ;default
-          ("doomdir" . "/home/flowmis/.doom.d/%s")                      ;default
-          ("emacsdir" . "/home/flowmis/.emacs.d/%s")                    ;default
-          ("doom" . "https://github.com/hlissner/doom-emacs/%s")        ;default
-          ("wolfram" . "https://wolframalpha.com/input/?i=%s")          ;default
-          ("wikipedia" . "https://en.wikipedia.org/wiki/%s")            ;default
-          ("duckduckgo" . "https://duckduckgo.com/?q=%s")               ;default
-          ("gmap" . "https://maps.google.com/maps?q=%s")                ;default
-          ("gimages" . "https://google.com/images?q=%s")                ;default
-          ("youtube" . "https://youtube.com/watch?v=%s")                ;default
-          ("github" . "https://github.com/%s")                          ;default
-          ("arch-wiki" . "https://wiki.archlinux.org/index.php/"))      ;selbst hinzugefügt
-      org-todo-keywords        ; This overwrites the default Doom org-todo-keywords
-        '((sequence
-           "TODO(t@!)"
-           "EILIG(e@/!)"
-           "PAUSIERT(p@/!)"
-           "|"                 ; The pipe necessary to separate "active" states and "inactive" states
-           "BEENDET(b@/!)"
-           "ABGEBROCHEN(a@/!)"
-           "DELEGIERT(d@/!)"))))
-(after! yas
-  :config
-  (yas/initialize)
-  (setq yas/root-directory "~/Dropbox/emacs/yasnippets/")
-  (yas/load-directory yas/root-directory)
-  (add-to-list 'load-path "~/Dropbox/emacs/yasnippets/")
-  (require 'yasnippet)
-  (setq yas-snippet-dirs '("~/Dropbox/emacs/yasnippets/"))             ;gleichen Dropbox Ordner angeben wie für Orga.org um dort die Snippets zu speichern
-  (yas-global-mode 1))
-(after! org
-  :init      ;; tweak dashboard config before loading it
-  (setq dashboard-set-heading-icons t)
-  (setq display-line-numbers nil)
-  (setq dashboard-set-file-icons t)
-  ;;(setq dashboard-startup-banner 'logo) ;; use standard emacs logo as banner
-  (setq dashboard-startup-banner "~/.config/doom/banner/banner.png")  ;; use custom image as banner
-  (setq dashboard-center-content nil) ;; set to 't' for centered content
-  (setq dashboard-items '((recents . 5)
+  :init
+  (setq user-full-name "Markus Hoffmann"
+        user-mail-address "manemarkushoffmann@gmail.com" ;GPG configuration, email clients, file templates and snippets,... can use this information - ist optional
+        undo-tree-auto-save-history t
+        undo-tree-history-directory-alist '(("." . "~/Dropbox/emacs/undo-tree-history"))
+        save-interprogram-paste-before-kill t ; Speichert kopierte Inhalte ausserhalb Emacs in den kill ring und macht es leichter bei zwischenzeitlichem löschen innerhalb Emacs das kopierte doch einzufügen
+        org-log-into-drawer 1    ;Notes mit <C-c C-z> werden direkt in den Drawer :LOGBOOK: geschrieben wenn dieser vorhanden ist
+        eshell-rc-script "~/.config/doom/eshell/profile"
+        eshell-aliases-file "~/.config/doom/eshell/aliases"
+        eshell-buffer-maximum-lines 5000
+        eshell-scroll-to-bottom-on-input t
+        org-directory "~/Dropbox/emacs/org-roam/Notizen/"
+        org-agenda-files '("~/Dropbox/emacs/org-roam/Notizen/orga/agenda.org"))
+  :dashboard
+  (setq dashboard-set-heading-icons t
+        display-line-numbers nil
+        dashboard-set-file-icons t
+        ;; dashboard-startup-banner 'logo ;; use standard emacs logo as banner
+        dashboard-startup-banner "~/.config/doom/banner/banner.png"  ;; use custom image as banner
+        dashboard-center-content nil ;; set to 't' for centered content
+        dashboard-items '((recents . 5)
                           (agenda . 5)
                           (bookmarks . 3)
-                          (projects . 3)))
-  (setq doom-fallback-buffer-name "*dashboard*")
+                          (projects . 3))
+        doom-fallback-buffer-name "*dashboard*")
   :config
   (dashboard-setup-startup-hook)
   (dashboard-modify-heading-icons '((recents . "file-text")
-                                    (bookmarks . "book"))))
+                                    (bookmarks . "book")))
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+  (set-frame-parameter (selected-frame) 'alpha '(95 . 70))   ;Zahl 1 nach alpha gibt Transparenz des aktiven Bildschirms und Zahl 2 gibt Transparenz wenn anderes Window im Focus ist
+  (custom-set-faces '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
+                    '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+                    '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+                    '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+                    '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
+  (beacon-mode 1)                 ;hilft den Cursor schnell zu finden durch aufblinken
+  ;; (setq display-line-numbers-type 'visual)
+  (setq display-line-numbers-type nil
+        doom-theme 'doom-molokai
+        doom-font (font-spec :family "Source Code Pro" :size 13)
+        doom-variable-pitch-font (font-spec :family "Fira Code" :size 13)
+        doom-big-font (font-spec :family "Source Code Pro" :size 20)
+        doom-unicode-font (font-spec :family "Source Code Pro" :size 10)
+        doom-serif-font (font-spec :family "Source Code Pro" :size 10)
+        org-ellipsis " ▼ "
+        org-hide-emphasis-markers t ;;macht dass Markierungssymbole um kursiv, dick, unterstrichen,... zu schreiben (~-_/*=) ausgeblendet werden
+        org-default-notes-file (expand-file-name "notes.org" org-directory)
+        org-log-done 'time
+        org-journal-dir "~/Dropbox/emacs/org-roam/Notizen/orga/journal/"
+        org-journal-date-format "%B %d, %Y (%A) "
+        org-journal-file-format "%Y-%m-%d.org"
+        org-todo-keywords '((sequence
+                             "TODO(t@!)"
+                             "EILIG(e@/!)"
+                             "PAUSIERT(p@/!)"
+                             "|"                 ; The pipe necessary to separate "active" states and "inactive" states
+                             "BEENDET(b@/!)"
+                             "ABGEBROCHEN(a@/!)"
+                             "DELEGIERT(d@/!)")))
+  (custom-set-faces!
+        '(font-lock-comment-face :slant italic)       ;Macht Kommentare wie diesen kursiv
+        '(font-lock-keyword-face :slant italic)))      ;Macht Keywords wie setq, after! ... kursiv
 (after! dired
   :hook
   (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
@@ -75,31 +73,27 @@
                               ("jpg" . "pinta")
                               ("png" . "viewnior")
                               ("mkv" . "vlc")
+                              ("html" . "brave")
                               ("mp4" . "vlc"))))
-
-(setq doom-font (font-spec :family "Source Code Pro" :size 13)
-     doom-variable-pitch-font (font-spec :family "Fira Code" :size 13)
-     doom-big-font (font-spec :family "Source Code Pro" :size 20)
-     doom-unicode-font (font-spec :family "Source Code Pro" :size 10)
-     doom-serif-font (font-spec :family "Source Code Pro" :size 10)
-     )
-(setq doom-theme 'doom-molokai)
-(custom-set-faces!
-  '(font-lock-comment-face :slant italic)       ;Macht Kommentare wie diesen kursiv
-  '(font-lock-keyword-face :slant italic))      ;Macht Keywords wie setq, after! ... kursiv
-(after! doom-themes
-      :config
-      (beacon-mode 1)                 ;hilft den Cursor schnell zu finden durch aufblinken
-      (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-      (set-frame-parameter (selected-frame) 'alpha '(95 . 70))   ;Zahl 1 nach alpha gibt Transparenz des aktiven Bildschirms und Zahl 2 gibt Transparenz wenn anderes Window im Focus ist
-      (custom-set-faces '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
-                        '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
-                        '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
-                        '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
-                        '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
-      ;; (setq display-line-numbers-type 'visual)
-      (setq display-line-numbers-type nil)
-      )
+(use-package yasnippet
+  :config
+  (setq yas-snippet-dirs '("~/Dropbox/emacs/yasnippets/"))             ;gleichen Dropbox Ordner angeben wie für Orga.org um dort die Snippets zu speichern
+  (yas-global-mode 1))
+(add-hook 'yas-minor-mode-hook(lambda()
+                                (yas-activate-extra-mode 'fundamental-mode)))
+(defun activate-ditaa-path ()
+ (interactive)
+ (setq org-ditaa-jar-path "/usr/share/java/ditaa/ditaa-0.11.jar"))
+(global-set-key (kbd "C-c m a") 'activate-ditaa-path)
+(use-package ox-reveal
+:ensure ox-reveal) ;Kann ich weglassen wenn ich Paket in packages.el lade?
+(setq org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js") ;"file:///home/flowmi/Git/pres/reveal" -> ist lokaler Pfad falls ich mein pres Repo ofline geclont habe
+(setq org-reveal-mathjax t)    ;math type ermöglicht - genauer einlesen
+(use-package htmlize
+:ensure t)                    ;sorgt dafür dass Syntax highlighting etc in der HTML angezeigt wird ----beim evaluieren kommt aber irgendwie dass es ignoriert wird also kein Plan?
+;Sonstige Exporteinstellungen
+(setq org-publish-use-timestamps-flag nil)
+(setq org-export-with-broken-links t)
 
 (global-set-key (kbd "M-v") 'er/expand-region) ;markiert bei jeder Wiederholung immer weiter nach aussen --> Macht es einem leicht bestimmte logische Bereiche schnell zu markieren
 (global-set-key (kbd "M-p") 'yank-from-kill-ring) ;zeigt kill ring - man kann auswählen was man von dem zuvor gekilltem einfügen will
@@ -148,8 +142,7 @@
        :desc "Point to register" "SPC" #'point-to-register)
       (:prefix ("b". "buffer")
        :desc "List bookmarks" "L" #'list-bookmarks
-       :desc "Save current bookmarks to bookmark file" "w" #'bookmark-save)
-)
+       :desc "Save current bookmarks to bookmark file" "w" #'bookmark-save))
 (evil-define-key 'normal dired-mode-map
   (kbd "M-RET") 'dired-display-file
   (kbd "h") 'dired-up-directory
@@ -231,8 +224,6 @@
 ;;         "* %^{Description}      Hinzugefügt am: %U      %^g")
 ;; ))
 
-(setq org-log-into-drawer 1)    ;Notes mit <C-c C-z> werden direkt in den Drawer :LOGBOOK: geschrieben wenn dieser vorhanden ist
-
 (defun pres-start ()
   (load-theme 'doom-henna)
   (menu-bar-mode -1)
@@ -268,123 +259,3 @@
 (use-package org-tree-slide
   :hook ((org-tree-slide-play . pres-start)
          (org-tree-slide-stop . pres-end)))
-
-;; (setq bibtex-completion-bibliography '("~/Dropbox/emacs/bib/references.bib")
-;; 	bibtex-completion-library-path '("~/Dropbox/emacs/bib/bibtex-pdfs/")
-;; 	bibtex-completion-notes-path "~/Dropbox/emacs/bib/notes/"
-;; 	bibtex-completion-notes-template-multiple-files "* ${author-or-editor}, ${title}, ${journal}, (${year}) :${=type=}: \n\nSee [[cite:&${=key=}]]\n"
-;; 	bibtex-completion-additional-search-fields '(keywords)
-;; 	bibtex-completion-display-formats
-;; 	'((article       . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${journal:40}")
-;; 	  (inbook        . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} Chapter ${chapter:32}")
-;; 	  (incollection  . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${booktitle:40}")
-;; 	  (inproceedings . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${booktitle:40}")
-;; 	  (t             . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*}"))
-;; 	bibtex-completion-pdf-open-function
-;; 	(lambda (fpath)
-;; 	  (call-process "open" nil 0 nil fpath)))
-;; (require 'bibtex)
-;; (setq bibtex-autokey-year-length 4
-;; 	bibtex-autokey-name-year-separator "-"
-;; 	bibtex-autokey-year-title-separator "-"
-;; 	bibtex-autokey-titleword-separator "-"
-;; 	bibtex-autokey-titlewords 2
-;; 	bibtex-autokey-titlewords-stretch 1
-;; 	bibtex-autokey-titleword-length 5
-;; 	org-ref-bibtex-hydra-key-binding (kbd "H-b"))
-;; (define-key bibtex-mode-map (kbd "H-b") 'org-ref-bibtex-hydra/body)
-;;  (require 'org-ref-ivy)
-;; (setq org-ref-insert-link-function 'org-ref-insert-link-hydra/body
-;;       org-ref-insert-cite-function 'org-ref-cite-insert-ivy
-;;       org-ref-insert-label-function 'org-ref-insert-label-link
-;;       org-ref-insert-ref-function 'org-ref-insert-ref-link
-;;       org-ref-cite-onclick-function (lambda (_) (org-ref-citation-hydra/body)))
-;; (setq org-latex-pdf-process (list "latexmk -shell-escape -bibtex -f -pdf %f"))
-
-(defun activate-ditaa-path ()
- (interactive)
- (setq org-ditaa-jar-path "/usr/share/java/ditaa/ditaa-0.11.jar"))
-(global-set-key (kbd "C-c m a") 'activate-ditaa-path)
-
-(setq undo-tree-auto-save-history t)
-(setq undo-tree-history-directory-alist '(("." . "~/Dropbox/emacs/undo-tree-history")))
-
-(pdf-tools-install t)                   ; Vll brauch ich es nicht, aber sollte dafür sorgen dass statt docview der pdfview aktiviert ist beim öffnen von pdfs sodass ich suchen, markieren etc. kann mit den pdf-tools!
-
-(setq save-interprogram-paste-before-kill t) ; Speichert kopierte Inhalte ausserhalb Emacs in den kill ring und macht es leichter bei zwischenzeitlichem löschen innerhalb Emacs das kopierte doch einzufügen
-
-;; (use-package multiple-cursors        ;Habe das Paket von hlissner in init.el aktiviert weil ich es besser finde!
-;;   :config
-;;   (setq mc/always-run-for-all 1)
-;;   (global-set-key (kbd "C-c m m") 'mc/mark-next-like-this)
-;;   (global-set-key (kbd "C-c m M") 'mc/mark-all-dwim)
-;;   (global-set-key (kbd "C-c m a") 'mc/edit-lines)
-;;   (global-set-key (kbd "C-c m q") 'mc/keyboard-quit)
-;;   (global-set-key (kbd "C-c m n") 'mc/skip-to-next-like-this)
-;;   (global-set-key (kbd "C-c m p") 'mc/skip-to-previous-like-this)
-;;   (define-key mc/keymap (kbd "<return>") nil))
-
-;; (setq ispell-program-name "aspell")   ;<z=> mit Cursor über Wort sagt dir schnell ob es richtig geschrieben ist! Kann aber auch wie folgt ein Keybinding für diese Funktion einrichten (habe in arch aspell(das neue ispell) installiert mit entsprechendem Wörterbuch)
-;; (global-set-key (kbd "C-c f") 'ispell-word)  ;mit diesem Befehl kann man leicht keybindings setzen für alle Funktionen die man mit <M-x> finden kann -> man muss jedoch
-
-;; ;; Rechtschreibprüfung vorerst aus weil noch nicht auf deutsch
-;; (remove-hook 'text-mode-hook #'spell-fu-mode)
-;; (setq global-spell-fu-mode 0)
-
-
-
-;; (after! neotree
-;;   (setq neo-smart-open t
-;;         neo-window-fixed-size nil))
-;; (after! doom-themes
-;;   (setq doom-neotree-enable-variable-pitch t))
-;; (map! :leader
-;;       :desc "Toggle neotree file viewer" "t n" #'neotree-toggle
-;;       :desc "Open directory in neotree" "d n" #'neotree-dir)
-
-;; (setq centaur-tabs-set-bar 'over
-;;       centaur-tabs-set-icons t
-;;       centaur-tabs-gray-out-icons 'buffer
-;;       centaur-tabs-height 24
-;;       centaur-tabs-set-modified-marker t
-;;       centaur-tabs-style "bar"
-;;       centaur-tabs-modified-marker "•")
-;; (map! :leader
-;;       :desc "Toggle tabs globally" "t c" #'centaur-tabs-mode
-;;       :desc "Toggle tabs local display" "t C" #'centaur-tabs-local-mode)
-;; (evil-define-key 'normal centaur-tabs-mode-map (kbd "g <right>") 'centaur-tabs-forward        ; default Doom binding is 'g t'
-;;                                                (kbd "g <left>")  'centaur-tabs-backward       ; default Doom binding is 'g T'
-;;                                                (kbd "g <down>")  'centaur-tabs-forward-group
-;;                                                (kbd "g <up>")    'centaur-tabs-backward-group)
-
-;; (setq ivy-posframe-display-functions-alist
-;;       '((swiper                     . ivy-posframe-display-at-point)
-;;         (complete-symbol            . ivy-posframe-display-at-point)
-;;         (counsel-M-x                . ivy-display-function-fallback)
-;;         (counsel-esh-history        . ivy-posframe-display-at-window-center)
-;;         (counsel-describe-function  . ivy-display-function-fallback)
-;;         (counsel-describe-variable  . ivy-display-function-fallback)
-;;         (counsel-find-file          . ivy-display-function-fallback)
-;;         (counsel-recentf            . ivy-display-function-fallback)
-;;         (counsel-register           . ivy-posframe-display-at-frame-bottom-window-center)
-;;         (nil                        . ivy-posframe-display))
-;;       ivy-posframe-height-alist
-;;       '((swiper . 20)
-;;         (t . 10)))
-;; (ivy-posframe-mode 1) ; 1 enables posframe-mode, 0 disables it.
-
-;; (map! :leader
-;;       (:prefix ("v" . "Ivy")
-;;        :desc "Ivy push view" "v p" #'ivy-push-view
-;;        :desc "Ivy switch view" "v s" #'ivy-switch-view))
-
-;HTML PRÄSENTATIONEN
-(use-package ox-reveal
-:ensure ox-reveal) ;Kann ich weglassen wenn ich Paket in packages.el lade?
-(setq org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js") ;"file:///home/flowmi/Git/pres/reveal" -> ist lokaler Pfad falls ich mein pres Repo ofline geclont habe
-(setq org-reveal-mathjax t)    ;math type ermöglicht - genauer einlesen
-(use-package htmlize
-:ensure t)                    ;sorgt dafür dass Syntax highlighting etc in der HTML angezeigt wird ----beim evaluieren kommt aber irgendwie dass es ignoriert wird also kein Plan?
-;Sonstige Exporteinstellungen
-(setq org-publish-use-timestamps-flag nil)
-(setq org-export-with-broken-links t)
--- tangle/packages.el
+++ tangle/packages.el
@@ -1,70 +1,29 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-;; To install a package with Doom you must declare them here and run 'doom sync'
-;; on the command line, then restart Emacs for the changes to take effect -- or
-;; use 'M-x doom/reload'.
-
-
-;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-;(package! some-package)
-
-;; To install a package directly from a remote git repo, you must specify a
-;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
-;; https://github.com/radian-software/straight.el#the-recipe-format
-;(package! another-package
-;  :recipe (:host github :repo "username/repo"))
-
-;; If the package you are trying to install does not contain a PACKAGENAME.el
-;; file, or is located in a subdirectory of the repo, you'll need to specify
-;; `:files' in the `:recipe':
-;(package! this-package
-;  :recipe (:host github :repo "username/repo"
-;           :files ("some-file.el" "src/lisp/*.el")))
-
-;; If you'd like to disable a package included with Doom, you can do so here
-;; with the `:disable' property:
-;(package! builtin-package :disable t)
-
-;; You can override the recipe of a built in package without having to specify
-;; all the properties for `:recipe'. These will inherit the rest of its recipe
-;; from Doom or MELPA/ELPA/Emacsmirror:
-;(package! builtin-package :recipe (:nonrecursive t))
-;(package! builtin-package-2 :recipe (:repo "myfork/package"))
-
-;; Specify a `:branch' to install a package from a particular branch or tag.
-;; This is required for some packages whose default branch isn't 'master' (which
-;; our package manager can't deal with; see radian-software/straight.el#279)
-;(package! builtin-package :recipe (:branch "develop"))
-
-;; Use `:pin' to specify a particular commit to install.
-;(package! builtin-package :pin "1a2b3c4d5e")
-
-
-;; Doom's packages are pinned to a specific commit and updated from release to
-;; release. The `unpin!' macro allows you to unpin single packages...
-;(unpin! pinned-package)
-;; ...or multiple packages
-;(unpin! pinned-package another-pinned-package)
-;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
-;(unpin! t)
 ;; (package! gitconfig-mode
 ;; 	  :recipe (:host github :repo "magit/git-modes"
 ;; 			 :files ("gitconfig-mode.el")))
 ;; (package! gitignore-mode
 ;; 	  :recipe (:host github :repo "magit/git-modes"
 ;; 			 :files ("gitignore-mode.el")))
+(package! dashboard)
+(package! dired-open)
+(package! org-bullets)
+(package! org-tree-slide)
+(package! ox-reveal)
+(package! beacon)
+(package! centered-cursor-mode) ;für zentrales scrollen im Präsi Modus
+(package! org-drill) ;für zentrales scrollen im Präsi Modus
+(package! rg)
 ;; (package! flycheck-aspell)
 ;; (package! async)
 ;; (package! calfw)
 ;; (package! calfw-org)
-(package! dashboard)
-(package! dired-open)
 ;; (package! elpher)
 ;; (package! emojify)
 ;; (package! evil-tutor)
 ;; (package! ivy-posframe)
-(package! org-bullets)
 ;; (package! ox-gemini)
 ;; (package! ox-man)
 ;; (package! ox-gemini)
@@ -76,8 +35,6 @@
 ;; (package! tldr)
 ;; (package! wc-mode)
 ;; (package! hide-mode-line)
-(package! org-tree-slide)
-(package! ox-reveal)
 ;; (package! org-ref)
 ;; (package! ivy-bibtex)
 ;; (package! org-roam)
@@ -86,10 +43,6 @@
 ;; (package! org-noter)
 ;; (package! org-pdftools)
 ;; (package! org-super-agenda)
-(package! beacon)
-(package! centered-cursor-mode) ;für zentrales scrollen im Präsi Modus
-(package! org-drill) ;für zentrales scrollen im Präsi Modus
-(package! rg)
 ;; (package! eyebrowse)
 ;; (package! powerthesaurus) ;geht bisher nur auf Englisch
 ;; (package! synosaurus) ;geht bisher nicht
